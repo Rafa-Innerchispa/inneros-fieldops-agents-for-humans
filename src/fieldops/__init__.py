70a3a28d7a509acd05bc157381611a8a3bc4420f
Rafael Lopez <rafagye@gmail.com>
2026-09-12 21:58:21 -0500

    feat: expose FieldOps package API

---
+++ src/fieldops/__init__.py
@@ -0,0 +1,23 @@
+"""InnerOS FieldOps reusable execution layer."""
+
+from .contracts import (
+    ActionRequest,
+    ApprovalArtifact,
+    ApprovalStatus,
+    EvidenceReceipt,
+    ExecutionResult,
+    VerificationResult,
+)
+from .workflow import ApprovalDenied, ApprovalRequired, run_action
+
+__all__ = [
+    "ActionRequest",
+    "ApprovalArtifact",
+    "ApprovalStatus",
+    "EvidenceReceipt",
+    "ExecutionResult",
+    "VerificationResult",
+    "ApprovalDenied",
+    "ApprovalRequired",
+    "run_action",
+]
